efaae37ea406b165342deb8a57e62b8f8f697b07
DarcilioWeb <133403780+DarcilioWeb@users.noreply.github.com>
2024-03-06 13:44:14 -0300

    Update tarefa1.js
    correçao

--- tarefa1.js
+++ tarefa1.js
@@ -1,7 +1,7 @@
 var nome = "Darcilio";
 const cpf = "48327-4987132-32";
-var sobreNome0 = function tarefa1() {
+function tarefa1() {
     let sobreNome = "Gomes";
-    console.log(sobreNome)
+    console.log(`${nome}\n${sobreNome}\n${cpf}`)
 }
-console.log(nome,"\n",sobreNome0,"\n",cpf)
+tarefa1();
